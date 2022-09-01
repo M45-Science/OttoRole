95f9f0a87d4eff65300d70c4f40ab74fcf0e145e
Distortions81 <carlotto81@gmail.com>
2022-09-01 15:56:15 -0600

    Epoch Comment

--- cons/cons.go
+++ cons/cons.go
@@ -8,9 +8,14 @@ const (
 	MaxDiscordAttempts = 50
 	ClusterSize        = 100000
 	NumClusters        = 1000
+
 	/*GMT Thu Sep 01 2022 06:00:00 GMT+0000*/
 	RoleKeeperEpoch = 1662012000
-	RecordEnd       = 0x21
+	/* 32bit records will roll over at */
+	/* GMT: Wednesday, March 16, 2242 12:56:32 PM */
+	/* So we need to remember to upgrade this in 220 years ;) */
+
+	RecordEnd = 0x21
 
 	//Test only
 	TSize = 6543
